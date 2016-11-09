46a6a6934422501fcb64cd6a78f7d333f724ccf5
Keshi <823424123@163.com>
2016-11-09 20:05:57 +0800

    Update the FusionChess Class.

--- app/src/main/java/com/keshi/chess/FusionChess.java
+++ app/src/main/java/com/keshi/chess/FusionChess.java
@@ -1,7 +1,6 @@
 package com.keshi.chess;
 
 import android.graphics.Point;
-
 import java.util.Stack;
 
 public class FusionChess {
@@ -14,31 +13,48 @@ public class FusionChess {
     public static final byte CHARIOT=5;     // 车
     public static final byte CANNON=6;      // 炮
     public static final byte PAWN=7;        // 卒
-    // Set static member about typeColor.
-    public static final byte TYPE_RED=0;    // 红
-    public static final byte TYPE_BLACK=1;  // 黑
+    public static final byte HORSE_HORSE=8;     // 双马
+    public static final byte HORSE_CHARIOT=9;   // 马车
+    public static final byte HORSE_CANNON=10;   // 马炮
+    public static final byte HORSE_PAWN=11;     // 马卒
+    public static final byte CHARIOT_CHARIOT=12;// 双车
+    public static final byte CHARIOT_PAWN=13;   // 车卒
+    public static final byte CANNON_CHARIOT=14; // 炮车
+    public static final byte CANNON_CANNON=15;  // 双炮
+    public static final byte CANNON_PAWN=16;    // 炮卒
+    public static final byte PAWN_PAWN=17;      // 双卒
     // Set static default chessboard.
     private static final byte defaultBoard[][]= {
             {   CHARIOT,    HORSE,      ELEPHANT,   ADVISER,    KING},
             {   BLANK,      CANNON,     BLANK,      BLANK,      BLANK},
             {   PAWN,       BLANK,      PAWN,       BLANK,      PAWN}};
+    // Set static fusion-pieces array.(x,y)->HORSE,CHARIOT,CANNON,PAWN
+    private static final byte fusionPieces[][]={
+            {HORSE_HORSE},      {HORSE_CHARIOT},    {HORSE_CANNON},     {HORSE_PAWN},
+            {HORSE_CHARIOT},    {CHARIOT_CHARIOT},  {CANNON_CHARIOT},   {CHARIOT_PAWN},
+            {HORSE_CANNON},     {HORSE_PAWN},       {CANNON_CANNON},    {CANNON_PAWN},
+            {HORSE_PAWN},       {CHARIOT_PAWN},     {CANNON_PAWN},      {PAWN_PAWN}};
+    // Set static divide-pieces array.(x=x-8:HORSE_CHARIOT...)
+    private static final byte dividePieces[][]={
+            {HORSE,HORSE},      // x=0=HORSE_HORSE
+            {HORSE,CHARIOT},    // x=1=HORSE_CHARIOT ...
+            {HORSE,CANNON},{HORSE,PAWN},{CHARIOT,CHARIOT},
+            {CHARIOT,PAWN},{CANNON,PAWN},{CANNON,CANNON},{CANNON,PAWN},{PAWN,PAWN}};
     // ChessBoard is used to save values about chess information.
     private byte chessBoard[][]=new byte[10][9];
     // The Stack is for Saving data of records.
-    Stack pieceRecordsStack=new Stack<ChessRecord>();
+    Stack<ChessRecord> chessRecordsStack= new Stack<ChessRecord>();
     // Initialize class FusionChess.
     public FusionChess(){
-        IntiChessBoard(TYPE_RED);
-    }
-    public FusionChess(byte myType){
-        IntiChessBoard(myType);
-
+        initChessBoard();
     }
 
-    public byte getChessBoard(int x,int y,byte myType) {
-        if(myType==TYPE_BLACK){
-            x=10-x; y=9-y;
-        }
+    /**
+     * This function is used to get pieces on chessboard.
+     * @param x The location.x of piece
+     * @param y The location.y of piece
+     */
+    public byte getChessBoard(int x,int y) {
         return chessBoard[x][y];
     }
 
@@ -47,25 +63,84 @@ public class FusionChess {
      * @param x The location.x of piece
      * @param y The location.y of piece
      * @param piece The type of piece.
-     * @param type  The type of color. (TYPE_RED or TYPE_BLACK)
      */
-    public void setChessBoard(int x,int y,byte piece,byte type) {
-        if(type==TYPE_BLACK){
-            x=10-x; y=9-y;
-            piece=(byte)(-piece);
-        }
+    public void setChessBoard(int x,int y,byte piece) {
         chessBoard[x][y] = piece;
     }
 
-    public void IntiChessBoard(byte myType){
-        byte enemyType=myType==TYPE_RED? TYPE_BLACK:TYPE_RED;
-        for(int i=0;i<10;i++){
-            setChessBoard(0,i,myType,defaultBoard[0][Math.abs(Math.abs(5-i)-5)]);
-            setChessBoard(0,i,enemyType,defaultBoard[0][Math.abs(Math.abs(5-i)-5)]);
-            setChessBoard(2,i,myType,defaultBoard[1][Math.abs(Math.abs(5-i)-5)]);
-            setChessBoard(2,i,enemyType,defaultBoard[1][Math.abs(Math.abs(5-i)-5)]);
-            setChessBoard(5,i,myType,defaultBoard[2][Math.abs(Math.abs(5-i)-5)]);
-            setChessBoard(5,i,enemyType,defaultBoard[2][Math.abs(Math.abs(5-i)-5)]);
+    /**
+     * This function is used to initialize Chessboard.
+     */
+    public void initChessBoard(){
+        for(int i=0;i<9;i++){
+            setChessBoard(0,i,defaultBoard[0][Math.abs(Math.abs(5-i)-5)]);
+            setChessBoard(1,i,BLANK);
+            setChessBoard(2,i,defaultBoard[1][Math.abs(Math.abs(5-i)-5)]);
+            setChessBoard(3,i,defaultBoard[2][Math.abs(Math.abs(5-i)-5)]);
+            setChessBoard(4,i,BLANK);
+            setChessBoard(5,i,BLANK);
+            setChessBoard(6,i,(byte)-defaultBoard[2][Math.abs(Math.abs(5-i)-5)]);
+            setChessBoard(7,i,(byte)-defaultBoard[1][Math.abs(Math.abs(5-i)-5)]);
+            setChessBoard(8,i,BLANK);
+            setChessBoard(9,i,(byte)-defaultBoard[0][Math.abs(Math.abs(5-i)-5)]);
+        }
+        chessRecordsStack.clear(); // Clear stack.
+    }
+
+    /**
+     * This function is used to judge if a moving is legal.
+     * @param from The location of piece that from.
+     * @param to The Location of piece that to.
+     * @return YES or NOT
+     */
+
+    /**
+     * This function is to move pieces.
+     * @param from The location of piece that from.
+     * @param to The Location of piece that to.
+     * @param choosePiece The piece which you want to move.
+     */
+    public void movePiece(Point from,Point to,byte choosePiece){
+        byte fromPiece=getChessBoard(from.x,from.y);
+        byte toPiece=getChessBoard(to.x,to.y);
+        chessRecordsStack.push(new ChessRecord(from,to,choosePiece,toPiece));
+
+        if(choosePiece==fromPiece)  // Move pieces
+            setChessBoard(from.x,from.y,BLANK);
+        else {   //Divide pieces
+            fromPiece-=8;
+            setChessBoard(from.x, from.y, dividePieces[fromPiece][0] != choosePiece ?
+                    dividePieces[fromPiece][0] : dividePieces[fromPiece][1]);
+        }
+        if(choosePiece*toPiece<=0)    // Eat pieces
+            setChessBoard(to.x,to.y,choosePiece);
+        else{   //Fusion pieces
+            choosePiece-=4; toPiece-=4;
+            setChessBoard(to.x,to.y,fusionPieces[choosePiece-4][toPiece-4]);
+        }
+
+    }
+
+    /**
+     * This function is to undo pieces.
+     */
+    public void undoPiece(){
+        if(chessRecordsStack.empty())
+            return; // Return when the stack is empty.
+
+        ChessRecord record=chessRecordsStack.pop();
+        Point from=record.getFrom();
+        Point to=record.getTo();
+        byte fromPiece=getChessBoard(from.x,from.y);
+        byte choosePiece=record.getChoosePiece();
+        byte toPiece=record.getToPiece();
+
+        setChessBoard(to.x,to.y,toPiece);
+        if(fromPiece==BLANK)
+            setChessBoard(from.x,from.y,choosePiece);
+        else{   //Fusion pieces
+            choosePiece-=4; fromPiece-=4;
+            setChessBoard(from.x,from.y,fusionPieces[choosePiece-4][fromPiece-4]);
         }
     }
 }
@@ -74,12 +149,12 @@ public class FusionChess {
 class ChessRecord{
     private Point from;
     private Point to;
-    private byte fromPiece,toPiece;
+    private byte choosePiece,toPiece;
 
     public ChessRecord(Point from,Point to,byte fromPiece,byte toPiece){
         setFrom(from);
         setTo(to);
-        setFromPiece(fromPiece);
+        setChoosePiece(fromPiece);
         setToPiece(fromPiece);
     }
 //    public ChessRecord(int fromX,int fromY,int toX,int toY,byte fromPiece,byte toPiece){
@@ -102,12 +177,12 @@ class ChessRecord{
         this.to = to;
     }
 
-    public byte getFromPiece() {
-        return fromPiece;
+    public byte getChoosePiece() {
+        return choosePiece;
     }
 
-    public void setFromPiece(byte fromPiece) {
-        this.fromPiece = fromPiece;
+    public void setChoosePiece(byte fromPiece) {
+        this.choosePiece = fromPiece;
     }
 
     public byte getToPiece() {
